b2b1e9984422c9753ad6f76ded5cd2a745c1f8b7
EC2 Default User <ec2-user@ip-172-31-38-234.ap-south-1.compute.internal>
2021-09-05 07:22:30 +0000

    changed content

--- js/new-age.js
+++ js/new-age.js
@@ -45,15 +45,15 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("comingSoon");
+// var btn = document.getElementById("comingSoon");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
